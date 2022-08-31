814ebae306d158073365ddeb70827865d7149f13
AleksandrMarkitan <0688138@gmail.com>
2022-08-31 18:45:14 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -20,21 +20,21 @@ function onAmountHandler(e) {
   amountOfElements = e.target.value;
 }
 
+let sideLength = 20;
 function createBoxes(amount) {
   let markup = ``;
-  let width = 20;
-  let height = 20;
+
   for (let index = 0; index < amount; index += 1) {
-    width += 10;
-    height += 10;
+    sideLength += 10;
     const color = getRandomHexColor();
-    markup += `<div style = "width: ${width}px; height: ${height}px; background-color: ${color};"></div>`;
+    markup += `<div style = "width: ${sideLength}px; height: ${sideLength}px; background-color: ${color};"></div>`;
   }
   refs.divBox.insertAdjacentHTML('beforeend', markup);
 }
 
 function destroyBoxes() {
   refs.divBox.innerHTML = '';
+  sideLength = 20;
 }
 
 function getRandomHexColor() {
